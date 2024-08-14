decf2a3108a2cbf090a71fc293c74d9d6e488e3f
anquetos <t.anquetil@proton.me>
2024-08-14 12:11:52 +0200

    Add display of events on map.

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from datetime import datetime
 import pandas as pd
 import streamlit as st
 from streamlit_folium import st_folium
+from geopy import distance
 
-from utils.folium_map import create_folium_map, add_selected_site_marker
+from utils.folium_map import (
+    create_folium_map,
+    add_selected_site_marker,
+    add_event_marker,
+)
 
 
 # ---------- Page configuration
@@ -65,17 +70,21 @@ with st.sidebar:
         value=False,
     )
 
+    if hide_sites:
+        df_sites = df_sites.loc[df_sites["end_date"].dt.date >= datetime.now().date()]
+
     games_type = st.radio(
         "Type de jeux", ["Olympiques", "Paralympiques"], horizontal=True
     )
 
-if hide_sites:
-    df_sites = df_sites.loc[df_sites["end_date"].dt.date >= datetime.now().date()]
+    if games_type == "Olympiques":
+        filtered_df = df_sites.loc[df_sites["games_type"] == "olympic"]
+    else:
+        filtered_df = df_sites.loc[df_sites["games_type"] == "paralympic"]
 
-if games_type == "Olympiques":
-    filtered_df = df_sites.loc[df_sites["games_type"] == "olympic"]
-else:
-    filtered_df = df_sites.loc[df_sites["games_type"] == "paralympic"]
+    events_distance = st.slider(
+        label="Distance (km) des événements", max_value=20, value=2
+    )
 
 # with st.expander("DataFrame"):
 #     st.dataframe(filtered_df)
@@ -125,7 +134,21 @@ selected_site = st.radio(
     captions=format_available_sites_information(),
 )
 
-# st.write(selected_site)
+
+# Compute distance between selected site and events and select events according to the 
+# selected distance in sidebar options.
+events = df_events.copy()
+events["distance_km"] = events.apply(
+    lambda x: distance.distance(
+        [x["latitude"], x["longitude"]], [selected_site["lat"], selected_site["lon"]]
+    ).km,
+    axis="columns",
+)
+events = events.sort_values(by=["distance_km"])
+events = events.loc[events["distance_km"] <= events_distance].to_dict(orient="records")
+
+
+# st.write(events)
 
 # Initialize Folium map
 m = create_folium_map(location=[selected_site["lat"], selected_site["lon"]])
@@ -134,8 +157,13 @@ m = create_folium_map(location=[selected_site["lat"], selected_site["lon"]])
 marker = add_selected_site_marker(site=selected_site)
 marker.add_to(m)
 
-# Fit Folium map bounds to nearest stations coordinates
-# m.fit_bounds([[site["lat"], site["lon"]] for site in available_sites])
+# Add markers on the Folium map for the nearest events
+for event in events:
+    marker = add_event_marker(event=event)
+    marker.add_to(m)
+
+# Fit Folium map bounds to nearest events coordinates
+m.fit_bounds([[event["latitude"], event["longitude"]] for event in events])
 
 # Render Folium map
 st_data = st_folium(m, use_container_width=True)
